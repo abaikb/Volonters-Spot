b88ad8f9d0ba40d374268f987a08dc459000b01b
abaikb <abaikb@icloud.com>
2023-05-01 19:10:51 +0600

    add validation signup

--- src/component/login/index.jsx
+++ src/component/login/index.jsx
@@ -1,45 +1,76 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
 import "./login.css";
 
 const Login = () => {
+  const loginSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("Введите логин")
+      .min(5, "Логин должен содержать минимум 5 символов"),
+    password: Yup.string()
+      .required("Введите пароль")
+      .min(5, "Пароль должен содержать минимум 5 символов"),
+  });
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
-  const handleSubmit = (event) => {
+ 
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Username:", username);
-    console.log("Password:", password);
+
+    try {
+      await loginSchema.validate({ username, password }, { abortEarly: false });
+      console.log("Username:", username);
+      console.log("Password:", password);
+      setErrors({});
+    } catch (validationErrors) {
+      const newErrors = {};
+      validationErrors.inner.forEach((error) => {
+        newErrors[error.path] = error.message;
+      });
+      setErrors(newErrors);
+    }
   };
 
   return (
-    <div className="wrapper">
-      <form onSubmit={handleSubmit} className="p-3 rounded shadow form-3d">
+    <div className="wrapper-login">
+      <form onSubmit={handleSubmit} className="p-3 rounded shadow form">
         <h1 className="text-center mb-4">Вход</h1>
         <div className="form-group">
           <input
             type="text"
             id="username"
-            className="form-control"
+            className={`form-control ${errors.username ? "is-invalid" : ""}`}
             placeholder="Логин"
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           />
+          {errors.username && (
+            <div className="invalid-feedback">{errors.username}</div>
+          )}
         </div>
         <div className="form-group">
           <input
             type="password"
             id="password"
-            className="form-control"
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
             placeholder="Пароль"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
+          {errors.password && (
+            <div className="invalid-feedback">{errors.password}</div>
+          )}
         </div>
         <button type="submit" className="btn btn-primary btn-block mt-4">
           Войти
         </button>
         <div className="link">
-          Нет аккаунта?<a href="#">Зарегистрируйся</a>
+        Нет аккаунта?<Link to="/signup">Зарегистрируйся</Link>
         </div>
       </form>
     </div>
