60ab020e3f1188bf65c93f12d0be046420c20bf8
abaikb <abaikb@icloud.com>
2023-05-03 17:19:06 +0600

    add item event

--- src/component/event-item/index.jsx
+++ src/component/event-item/index.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 export const EventItem = ({ event }) => {
   return (
-    <div>
-      <div>{event.name}</div>
-      <Link to={`/events/${event.id}`}>more info</Link>
-    </div>
+      <div className="event-list">
+        <div className="event-item">
+          <div className="event-title">{event.name}</div>
+          <img className="event-img" src={event.image} alt="" />
+          <p className="title">{event.desc}</p>
+          <Link to={`/events/${event.id}`}>подробнее</Link>
+        </div>
+      </div>
   );
 };
--- src/pages/events/index.jsx
+++ src/pages/events/index.jsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react"
-import { mockFetch } from "../../utils/mockFetch"
-import { EventItem } from '../../component/event-item'
-import './style.css'
+import { useEffect, useState } from "react";
+import { mockFetch } from "../../utils/mockFetch";
+import { EventItem } from "../../component/event-item";
+import "./style.css";
 
 export const EventsPage = () => {
-  const [events, setEvents] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    setLoading(true)
-    mockFetch('/events.json')
-      .then(res => res.json())
-      .then(data => {
-        setEvents(data)
+    setLoading(true);
+    mockFetch("/events.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setEvents(data);
       })
       .catch((err) => {
-        setError('произошла ошибка')
+        setError("произошла ошибка");
       })
       .finally(() => {
-        setLoading(false)
-      })
-  }, [])
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      {loading && <div>Loading...</div>}
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {
-        events.map((e) => (
-          <EventItem key={e.id} event={e}/>
-        ))
-      }
+      <div className="box">
+        {loading && <div>Loading...</div>}
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        {events.map((e) => (
+          <EventItem key={e.id} event={e} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
